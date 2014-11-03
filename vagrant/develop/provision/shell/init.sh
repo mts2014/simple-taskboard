98a105681ac3cc61fa96f50ca686462327c39dc7
mts <mts@mts>
2014-11-04 08:35:50 +0900

    m

--- vagrant/develop/provision/shell/init.sh
+++ vagrant/develop/provision/shell/init.sh
@@ -3,7 +3,7 @@ sudo apt-get -y install python-dev python-setuptools
 sudo easy_install pip
 sudo pip install ansible
 
-develop_provision_path = /home/vagrant/share/develop/provision
+develop_provision_path=/home/vagrant/share/develop/provision
 
 sudo cp $develop_provision_path/shell/insecure_private_key /home/vagrant/.ssh/insecure_private_key
 sudo cp $develop_provision_path/shell/ssh_config /home/vagrant/.ssh/config
