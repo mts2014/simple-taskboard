a88d9ad6194e4cebe050db31ead9e3862bbbdd1d
mts <mts@mts>
2014-11-12 12:55:12 +0900

    m

--- vagrant/develop/provision/shell/init.sh
+++ vagrant/develop/provision/shell/init.sh
@@ -7,6 +7,7 @@ develop_provision_path=/home/vagrant/share/develop/provision
 
 sudo cp $develop_provision_path/shell/insecure_private_key /home/vagrant/.ssh/insecure_private_key
 sudo cp $develop_provision_path/shell/ssh_config /home/vagrant/.ssh/config
+sudo cp $develop_provision_path/shell/.ansible.cfg /home/vagrant/.ansible.cfg
 
 ansible-playbook -i $develop_provision_path/ansible/hosts $develop_provision_path/ansible/init.yml
 
