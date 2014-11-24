683d97caa869270d66df9166a596ae8a59339938
mts <mts@mts>
2014-11-24 12:09:03 +0900

    m

--- vagrant/build-aws/provision/shell/init.sh
+++ vagrant/build-aws/provision/shell/init.sh
@@ -13,7 +13,7 @@ git remote add origin https://github.com/mts2014/simple-taskboard.git
 git pull origin master
 
 build_provision_path=/home/ubuntu/products/simple-taskboard/vagrant/build-aws/provision
-sudo cp $build_provision_path/shell/insecure_private_key /home/ubuntu/.ssh/insecure_private_key
+sudo cp $build_provision_path/shell/vagrant-aws.pem /home/ubuntu/.ssh/vagrant-aws.pem
 sudo cp $build_provision_path/shell/ssh_config /home/ubuntu/.ssh/config
 sudo cp $build_provision_path/shell/.ansible.cfg /home/ubuntu/.ansible.cfg
 ansible-playbook -i $build_provision_path/ansible/hosts $build_provision_path/ansible/init.yml
