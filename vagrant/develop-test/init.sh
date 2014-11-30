8e4e1e76534bf4c86905424d29418d82eb16294e
mts <mts@mts>
2014-11-30 22:43:22 +0900

    ansible 整理

---
+++ vagrant/develop-test/init.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get -y install python-dev python-setuptools
+sudo easy_install pip
+sudo pip install ansible
+
+# ansible実行時に鍵ファイルを正しく認識させるための設定
+chmod 0600 ~/.ssh/insecure_private_key
+ansible-playbook -i ~/ansible/develop ~/ansible/site.yml
