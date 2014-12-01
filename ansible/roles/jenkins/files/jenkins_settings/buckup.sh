79f682e198a80dde099b1a44f5cd6b0c521432c9
mts <mts@mts>
2014-12-02 00:25:33 +0900

    build 最適化

--- ansible/roles/jenkins/files/jenkins_settings/buckup.sh
+++ ansible/roles/jenkins/files/jenkins_settings/buckup.sh
@@ -3,6 +3,7 @@
 jenkins_home=/var/lib/jenkins
 home_setting_dir=$(pwd)/jenkins_home
 jobs_setting_dir=$(pwd)/jenkins_home/jobs
+plugin_buckup_dir=$1
 
 rm -fr $home_setting_dir/*.xml
 cp $jenkins_home/*.xml $home_setting_dir/
@@ -15,4 +16,5 @@ rm -fr $home_setting_dir/userContent/*
 cp $jenkins_home/userContent/* $home_setting_dir/userContent/
 
 rm -fr $home_setting_dir/plugins/*
-cp $jenkins_home/plugins/*.jpi $home_setting_dir/plugins/
+cp $jenkins_home/plugins/*.jpi $plugin_buckup_dir 
+
