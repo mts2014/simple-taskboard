456351c409ce4ac0043f49d4e04aa9598ced6472
mts <mts@mts>
2014-12-03 08:40:06 +0900

    ｍ

--- ansible/roles/jenkins/files/jenkins_settings/buckup.sh
+++ ansible/roles/jenkins/files/jenkins_settings/buckup.sh
@@ -4,7 +4,7 @@ base=$(cd $(dirname $0) && pwd)
 jenkins_home=/var/lib/jenkins
 home_setting_dir=$base/jenkins_home
 jobs_setting_dir=$base/jenkins_home/jobs
-plugin_buckup_dir=$(cd $(dirname $1) && pwd)
+plugin_buckup_dir=$(cd $1 && pwd)
 
 rm -fr $home_setting_dir/*.xml
 cp $jenkins_home/*.xml $home_setting_dir/
