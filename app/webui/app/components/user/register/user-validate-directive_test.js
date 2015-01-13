8f54183bb79e3806e1eb38396d7490561a0c0040
vagrant <vagrant@precise64.(none)>
2015-01-13 16:50:50 +0000

    m

--- app/webui/app/components/user/register/user-validate-directive_test.js
+++ app/webui/app/components/user/register/user-validate-directive_test.js
@@ -3,30 +3,30 @@
 describe('validate directive', function(){
         
   it('blurイベントでvalidateサービスの呼び出し', function(){
-    // var userService = jasmine.createSpyObj('userService', ['validate']);
-    // module('simple-taskboard.webui.components.user', function($provide){
-    //   $provide.value('userService', userService);
-    // });
-    // 
-    // var $compile, $rootScope, $q;
-    // inject(function(_$compile_, _$rootScope_, _$q_){
-    //   $compile = _$compile_;
-    //   $rootScope = _$rootScope_;
-    //   $q = _$q_;
-    // });
-    // 
-    // var defered = $q.defer(); 
-    // defered.resolve();
-    // userService.validate.and.returnValue(defered.promise);
-    // 
-    // var scope = $rootScope.$new();
-    // var input = $compile('<input type="text" ng-model="email" user-validate>')(scope);
-    // input.prop('value', 'taro@test');
-    // input.change();
-    // input.blur();
-    // 
-    // expect(userService.validate).toHaveBeenCalledWith(
-    //   jasmine.objectContaining({ email: "taro@test"}), ['email']);
+    var userService = jasmine.createSpyObj('userService', ['validate']);
+    module('simple-taskboard.webui.components.user', function($provide){
+      $provide.value('userService', userService);
+    });
+    
+    var $compile, $rootScope, $q;
+    inject(function(_$compile_, _$rootScope_, _$q_){
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      $q = _$q_;
+    });
+    
+    var defered = $q.defer(); 
+    defered.resolve();
+    userService.validate.and.returnValue(defered.promise);
+    
+    var scope = $rootScope.$new();
+    var input = $compile('<input type="text" ng-model="email" user-validate>')(scope);
+    input.prop('value', 'taro@test');
+    input.change();
+    input.blur();
+    
+    expect(userService.validate).toHaveBeenCalledWith(
+      jasmine.objectContaining({ email: "taro@test"}), ['email']);
     
   }); 
         
