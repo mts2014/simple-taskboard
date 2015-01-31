24585c6ba235767876e503da151a214544dcd2f2
mtn <nantsuka2011@gmail.com>
2015-02-01 00:47:08 +0900

    windowsでもrsyncでファイル同期できるようにする

--- vagrant/develop/init.sh
+++ vagrant/develop/init.sh
@@ -4,5 +4,5 @@ sudo easy_install pip
 sudo pip install ansible
 
 # ansible実行時に鍵ファイルを正しく認識させるための設定
-chmod 0600 ~/.ssh/insecure_private_key
+chmod 0600 ~/.ssh/private_key
 ansible-playbook -i ~/ansible/develop ~/ansible/site.yml
