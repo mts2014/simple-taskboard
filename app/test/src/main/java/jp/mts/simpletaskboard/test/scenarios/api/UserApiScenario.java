261ac57636edb5b78c8f542a05fc8105c89e279c
mtn <nantsuka2011@gmail.com>
2015-02-26 15:08:19 +0900

    remove duplicated test

--- app/test/src/main/java/jp/mts/simpletaskboard/test/scenarios/api/UserApiScenario.java
+++ app/test/src/main/java/jp/mts/simpletaskboard/test/scenarios/api/UserApiScenario.java
@@ -36,10 +36,10 @@ public class UserApiScenario {
 		ユーザを登録できること(){
 
 			userApi.登録する($in()
-					.v(EMAIL, "foo@test.jp")
-					.v(ユーザ名, "taro")
-					.v(パスワード, "pass")
-					.v(確認パスワード, "pass"));
+				.v(EMAIL,          "foo@test.jp")
+				.v(ユーザ名,       "taro")
+				.v(パスワード,     "pass")
+				.v(確認パスワード, "pass"));
 
 			userApi.存在する("foo@test.jp", "taro");
 		}
@@ -47,27 +47,6 @@ public class UserApiScenario {
 	}
 
 	public static class 登録情報の検証 extends 共通設定 {
-
-		@Test public void
-		すでに存在するメールアドレスの場合エラー(){
-
-			userApi.登録する($in()
-					.v(EMAIL, "baz@text.jp"));
-
-			userApi.登録時の検証をする($in()
-					.v(EMAIL, "baz@text.jp"))
-				.assertHasError("email", "指定されたメールアドレスはすでに登録されています。");;
-
-		}
-
-
-		@Test public void
-		存在しないメールアドレスの場合エラーにならない(){
-
-			userApi.登録時の検証をする($in()
-					.v(EMAIL, "not.exist@text.jp"))
-				.assertHasNoError();
-		}
 
 		@Test public void
 		メールアドレスが空の場合エラー(){
